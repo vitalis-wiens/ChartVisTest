595be5cd1db7c8f9f990801eefb7bb3af769ae29
Vitalis Wiens <vitalis.wiens@gmail.com>
2020-03-20 10:41:30 +0100

    Updates

---
+++ precache-manifest.45a74f7997b19d12fd96a94bf2b2f9db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851ff02ee7c8f94a3781b2fe9731d2db",
+    "url": "/ChartVisTest/index.html"
+  },
+  {
+    "revision": "18ad36151a77f5840e34",
+    "url": "/ChartVisTest/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ca3b4fa52727b6ca09f5",
+    "url": "/ChartVisTest/static/css/main.9e20bf5a.chunk.css"
+  },
+  {
+    "revision": "18ad36151a77f5840e34",
+    "url": "/ChartVisTest/static/js/2.a3809cf7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ChartVisTest/static/js/2.a3809cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3b4fa52727b6ca09f5",
+    "url": "/ChartVisTest/static/js/main.455524b7.chunk.js"
+  },
+  {
+    "revision": "b562967e32070f3985b8",
+    "url": "/ChartVisTest/static/js/runtime-main.8186a6b8.js"
+  },
+  {
+    "revision": "7d352eef4fc0d24048db63b8a4644f4e",
+    "url": "/ChartVisTest/static/media/logo.7d352eef.svg"
+  }
+]);
